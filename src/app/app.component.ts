4d25f525d95b8b97ceed467a6dd1564cb479b6db
mohamed <amine@viruscomm.ca>
2018-05-29 22:31:48 +0100

    update cage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component,ViewChild } from '@angular/core';
-import { Platform ,NavController} from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component, ViewChild} from '@angular/core';
+import {Platform, NavController} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
 import {Chat} from "../pages/chat/chat";
 import {CagesPage} from "../pages/cages/cages";
@@ -13,31 +13,33 @@ import {PostPage} from "../pages/post/post";
 import {AboutPage} from "../pages/about/about";
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+    rootPage: any = TabsPage;
 
-    birdPage=BirdsPage;
-    chat=ListUserChatPage;
-    cages=CagesPage;
-    myposts=PostPage;
-    myhome=TabsPage;
-    profile=ProfilePage;
-    aboutus=AboutPage;
+    birdPage = BirdsPage;
+    chat = ListUserChatPage;
+    cages = CagesPage;
+    myposts = PostPage;
+    myhome = TabsPage;
+    profile = ProfilePage;
+    aboutus = AboutPage;
     @ViewChild('contentRoot') content: NavController;
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
-    openPage(page,params?:any) {
+
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            statusBar.styleDefault();
+            splashScreen.hide();
+        });
+    }
+
+    openPage(page, params?: any) {
         // Reset the content nav to have just this page
         // we wouldn't want the back button to show in this scenario
-      this.content.setRoot(page,params);
+        this.content.setRoot(page, params);
         // if(document.querySelectorAll(".tabbar").length>0)
         // {
         //     let x=document.querySelectorAll(".tabbar")[0];
--- src/pages/cage-modal/cage-modal.ts
+++ src/pages/cage-modal/cage-modal.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 import {Storage} from "@ionic/storage";
+
 /**
  * Generated class for the CageModalPage page.
  *
@@ -10,55 +11,64 @@ import {Storage} from "@ionic/storage";
  * Ionic pages and navigation.
  */
 
-export class cageType{
-    _id:String;
-    owner:string;
-    number:number;
+export class cageType {
+    _id: String;
+    owner: string;
+    number: number;
+    male: String;
+    female: String;
 }
+
 @IonicPage()
 @Component({
-  selector: 'page-cage-modal',
-  templateUrl: 'cage-modal.html',
+    selector: 'page-cage-modal',
+    templateUrl: 'cage-modal.html',
 })
 export class CageModalPage {
-    couples=[];
-    cage:cageType;
-    addCage:Boolean;
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              private storage:Storage,
-              private http:HttpClient,
-              private viewCtrl:ViewController) {
+    cage: cageType;
+    addCage: Boolean;
+    userid: String;
+    birds: Array<any>;
 
-      this.cage=new cageType();
-      this.getCouples();
-      this.addCage = true;
-      if (navParams.get('cage')) {
-          this.addCage = false;
-          this.cage = navParams.get('cage');
-      }
-  }
-    getCouples() {
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                private storage: Storage,
+                private http: HttpClient,
+                private viewCtrl: ViewController) {
 
-        class resultData {
-            data:Array<any>
+        this.cage = new cageType();
+        this.addCage = true;
+        if (navParams.get('cage')) {
+            this.addCage = false;
+            this.cage = navParams.get('cage');
         }
-        let self = this;
-        new Promise(function (resolve) {
-
-            self.storage.get("userid").then(function (userid) {
-                self.http.get('http://localhost:8081/api/couples/user/' + userid).subscribe((res:resultData) => {
-                    let data = res;
-                    self.couples = data.data;
-                    console.log(data);
-                    resolve();
-                }, (err) => {
-                    console.log(err);
-                });
-            });
-        })
+        this.getUserId();
+        this.birds = [];
+        this.getBirds();
     }
 
+    getUserId() {
+        let self = this;
+        this.storage.get("userid").then(function (userID) {
+            self.userid = userID;
+        });
+    }
+
+    getBirds() {
+        class resultData {
+            data: Array<any>
+        }
+
+        let self = this;
+        this.storage.get("userid").then(function (userid) {
+            self.http.get('http://localhost:8081/api/birds/user/' + userid).subscribe((res: resultData) => {
+                self.birds = res.data;
+                console.log(res);
+            }, (err) => {
+                console.log(err);
+            });
+        });
+    }
     saveCage() {
 
         const httpOptions = {
@@ -72,7 +82,7 @@ export class CageModalPage {
             self.cage.owner = userID;
             self.cage.number = Number(self.cage.number);
             console.log(self.cage);
-            if(self.addCage){
+            if (self.addCage) {
 
                 self.http.post('http://localhost:8081/api/cages/', JSON.stringify(self.cage),
                     httpOptions).subscribe(res => {
@@ -83,9 +93,9 @@ export class CageModalPage {
                 }, (err) => {
                     console.log(err);
                 });
-            }else{
+            } else {
 
-                self.http.put('http://localhost:8081/api/cages/'+self.cage._id, JSON.stringify(self.cage),
+                self.http.put('http://localhost:8081/api/cages/' + self.cage._id, JSON.stringify(self.cage),
                     httpOptions).subscribe(res => {
                     console.log(res);
                     //
