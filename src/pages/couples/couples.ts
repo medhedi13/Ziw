5fe1c7b597c5ce46bf844e5137472b0e15007d15
mohamed <amine@viruscomm.ca>
2018-05-31 16:52:44 +0100

    add delete and update couples

--- src/pages/couples/couples.ts
+++ src/pages/couples/couples.ts
@@ -5,13 +5,21 @@ import {CouplesModalPage} from "../couples-modal/couples-modal";
 import {HttpClient} from "@angular/common/http";
 
 import {Storage} from "@ionic/storage";
+import {cageType} from "../cages/cages";
 /**
  * Generated class for the CouplesPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+export class coupleType{
+    _id:String;
+    male: String;
+    female: String;
+    owner:String;
+    name:String;
+    description:String;
+}
 @IonicPage()
 @Component({
   selector: 'page-couples',
@@ -36,19 +44,15 @@ export class CouplesPage {
         }
 
         let self = this;
-        new Promise(function (resolve) {
-
             self.storage.get("userid").then(function (userid) {
                 self.http.get('http://localhost:8081/api/couples/user/' + userid).subscribe((res: resultData) => {
                     let data = res;
                     self.couples = data.data;
                     console.log(data);
-                    resolve();
                 }, (err) => {
                     console.log(err);
                 });
             });
-        })
     }
     openModal() {
 
@@ -59,4 +63,27 @@ export class CouplesPage {
         modal.present();
     }
 
+    editCouple(obj:coupleType) {
+        let modal = this.modalCtrl.create(CouplesModalPage, {couple: obj});
+        modal.onDidDismiss(data => {
+                 this.getCouples();
+        });
+        modal.present();
+
+    }
+
+    deleteCouple(id) {
+        class resultData {
+            data: Array<any>
+        }
+
+        console.log(id);
+        this.http.delete('http://localhost:8081/api/couples/' + id).subscribe((res: resultData) => {
+                this.getCouples();
+            console.log(res);
+        }, (err) => {
+            console.log(err);
+        });
+
+    }
 }
